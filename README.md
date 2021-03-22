2d2212bb0c0cb7b206160e740282a50eef2eab14
Ivan Strilets <ivanstrilets@gmail.com>
2021-03-22 20:34:04 +0300

    init project && header

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Dijital_Project
+установка зависимостей:
+```
+npm i
+```
+
+запуск проекта:
+```
+npm run serve
+```
+
+сборка проекта:
+```
+npm run build
+```
+
+сборка проекта с минификацией файлов:
+```
+npm run build:min
+```
+
+особенности сброки:
+```
+Webpack 5
+EditorConfig
+Нативный JS: Babel
+Разметка: Pug
+Стили: Css / Scss / PostCSS
+```
+
+псевдонимы:
+```
+путь к папке images: ~img
+путь к папке fonts: ~fonts
+```
---
+++ src/imports/styles.js
@@ -0,0 +1,21 @@
+// common styles
+import '../styles/common/reset.scss'
+import '../styles/common/main.scss'
+
+// fonts styles
+import '../styles/fonts/montserrat.scss'
+
+// layouts styles
+import '../styles/layouts/example-layout.scss'
+
+// models styles
+import '../styles/models/example-model.scss'
+import '../styles/models/logo.scss'
+import '../styles/models/menu.scss'
+
+// views styles
+import '../styles/views/example-view.scss'
+import '../styles/views/header.scss'
+
+// pages styles
+import '../styles/pages/example-page.scss'
